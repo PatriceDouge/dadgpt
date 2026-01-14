d0855ca89cc4eeb69c84ba259594e6d6d39a44e6
Patrice Douge <patricedouge@gmail.com>
2026-01-13 21:33:31 -0500

    ralph setup improvements

--- ralph.sh
+++ ralph.sh
@@ -9,14 +9,7 @@ fi
 for ((i=1; i<=$1; i++)); do
   echo "=== Iteration $i of $1 ==="
 
-  result=$(claude --dangerously-skip-permissions -p "@IMPLEMENTATION_PLAN.md @progress.txt \
-  1. Find the highest-priority task and implement it. \
-  2. Run your tests and type checks. \
-  3. Update the prd.json to set `passes: true` for the completed story. \
-  4. Append your progress to progress.txt. \
-  5. Commit your changes. \
-  ONLY WORK ON A SINGLE TASK. \
-  If the implementation plan is complete, output <promise>COMPLETE</promise>.")
+  result=$(claude --dangerously-skip-permissions -p "@prompt.md @prd.json @progress.txt @IMPLEMENTATION_PLAN.md")
 
   echo "$result"
 
